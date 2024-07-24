d37d7e6978879ca94d3e6621c2c1f42d60796aaa
SAHILKUMARSAKSHI <sahilkumarsakshi_ec22a18_12@dtu.ac.in>
2024-07-24 20:05:58 +0530

    new commit

--- PERSONAL AI/VOICE ASSISTANCE.py
+++ PERSONAL AI/VOICE ASSISTANCE.py
@@ -23,7 +23,7 @@ from twilio.rest import Client
 import pickle
 import os.path
 
-# from googleapiclient.discovery import build
+from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from selenium import webdriver
--- PERSONAL AI/main.py
+++ PERSONAL AI/main.py
@@ -10,7 +10,7 @@ import smtplib
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-# print(voices[1].id)
+print(voices[1].id)
 engine.setProperty('voice', voices[0].id)
 
 
